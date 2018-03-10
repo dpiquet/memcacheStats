4b4a2888a33b580eddf9c18fa0cb6c9854a86322
Damien PIQUET <piqudam@gmail.com>
2018-03-10 13:51:17 +0100

    WIP: front

--- assets/js/vue-app.js
+++ assets/js/vue-app.js
@@ -3,6 +3,47 @@
  *
  */
 
+// Expose app
+var app;
+
+/**
+ * Create empty dataset
+ * @param title
+ * @returns {{labels: string[], datasets: *[]}}
+ */
+function getEmptyDataset(title)
+{
+    var backgroundColor = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ];
+
+    var borderColor = [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+
+    return {
+        labels: ["N/C"],
+        datasets: [{
+            label: title,
+            data: [NaN],
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            borderWidth: 1,
+            pointRadius: 0
+        }]
+    };
+}
+
 function globalOutOf(sourceData, index, comparedTo)
 {
     var ret = [];
@@ -22,6 +63,12 @@ function globalOutOf(sourceData, index, comparedTo)
     return ret;
 }
 
+/**
+ * Get instant values
+ * @param sourceData
+ * @param index
+ * @returns {Array}
+ */
 function instantValues(sourceData, index)
 {
     var ret = [];
@@ -43,94 +90,68 @@ function instantValues(sourceData, index)
     return ret;
 }
 
-window.addEventListener('load', function() {
+/**
+ * Get instant values as Kilobytes
+ * @param sourceData
+ * @param index
+ * @returns {Array}
+ */
+function instantKbValues(sourceData, index)
+{
+    var ret = [];
 
+    var previousVal = 0;
+    var processedVals = 0;
+
+    for (data in sourceData) {
+        var thisVal = Math.round(sourceData[data][index] / 1024).toFixed(2);
+
+        if (processedVals > 0) {
+            ret.push(thisVal - previousVal);
+        }
+
+        processedVals += 1;
+        previousVal = thisVal;
+    }
+
+    return ret;
+}
+
+/**
+ * Send memcached flush command
+ */
+function flushMemcached()
+{
+    axios.get('/flush').then(function() {
+        app.showModal = true;
+    });
+}
+
+/**
+ * Start app
+ */
+window.addEventListener('load', function() {
     Vue.use(VueChart);
 
-    var msg = 'test';
+    // register modal component
+    Vue.component('modal', {
+        template: '#modal-template'
+    });
 
-    var backgroundColor = [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ];
-
-    var borderColor = [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ];
-
-    var hitsVsMissDataset = {
-        labels: ["N/C"],
-        datasets: [{
-            label: 'Global % Get Hits',
-            data: [NaN],
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: 1
-        }]
-    };
-
-    var inVsOutDataset = {
-        labels: ["N/C"],
-        datasets: [{
-            label: 'Global % Bytes Read',
-            data: [NaN],
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: 1
-        }]
-    };
-
-    var getVsSetDataset = {
-        labels: ["N/C"],
-        datasets: [{
-            label: 'Global % get/set',
-            data: [NaN],
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: 1
-        }]
-    };
-
-    var instantHitsDataset = {
-        labels: ["N/C"],
-        datasets: [{
-            label: 'Instant Hits',
-            data: [NaN],
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: 1
-        }]
-    };
-
-    var instantMissesDataset = {
-        labels: ["N/C"],
-        datasets: [{
-            label: 'Instant Misses',
-            data: [NaN],
-            backgroundColor: backgroundColor,
-            borderColor: borderColor,
-            borderWidth: 1
-        }]
-    };
-
-    var app = new Vue({
+    app = new Vue({
         el: '#app',
         data: {
-            message: msg,
-            hitsVsMiss: hitsVsMissDataset,
-            inVsOut: inVsOutDataset,
-            getVsSet: getVsSetDataset,
-            instantHits: instantHitsDataset,
-            instantMisses: instantMissesDataset
+            showModal: false,
+            flushMemcached: flushMemcached,
+            hitsVsMiss: getEmptyDataset('Global % Get Hits'),
+            inVsOut: getEmptyDataset('Global % Bytes Read'),
+            getVsSet: getEmptyDataset('Global % get/set'),
+            instantHits: getEmptyDataset('Instant Get Hits'),
+            instantMisses: getEmptyDataset('Instant Get Misses'),
+            instantGetflushed: getEmptyDataset('Instant Get Flushed'),
+            instantGetexpired: getEmptyDataset('Instant Get Expired'),
+            instantBytesRead: getEmptyDataset('Instant Bytes Read'),
+            instantBytesWritten: getEmptyDataset('Instant Bytes Written')
         },
         mounted() {
             var root = this;
@@ -155,6 +176,22 @@ window.addEventListener('load', function() {
                     var instantMisses = instantValues(response.data, 'get_misses');
                     root.instantMisses.datasets[0].data = instantMisses;
                     root.instantMisses.labels = instantMisses;
+
+                    var chartdata = instantValues(response.data, 'get_flushed');
+                    root.instantGetflushed.datasets[0].data = chartdata;
+                    root.instantGetflushed.labels = chartdata;
+
+                    chartdata = instantKbValues(response.data, 'bytes_read');
+                    root.instantBytesRead.datasets[0].data = chartdata;
+                    root.instantBytesRead.labels = chartdata;
+
+                    chartdata = instantKbValues(response.data, 'bytes_written');
+                    root.instantBytesWritten.datasets[0].data = chartdata;
+                    root.instantBytesWritten.labels = chartdata;
+
+                    chartdata = instantKbValues(response.data, 'get_expired');
+                    root.instantGetexpired.datasets[0].data = chartdata;
+                    root.instantGetexpired.labels = chartdata;
                 });
             };
             setInterval(refresh, 3000);
