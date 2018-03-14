21ed688bb36cb8f0d204245ae1bf4655576f6725
Damien PIQUET <dpiquet@teicee.com>
2018-03-14 18:48:30 +0100

    Added charts

--- assets/js/vue-app.js
+++ assets/js/vue-app.js
@@ -128,6 +128,17 @@ function instantKbValues(sourceData, index)
     return ret;
 }
 
+function getRawValues(sourceData, index)
+{
+    var ret = [];
+
+    for (data in sourceData) {
+        ret.push(sourceData[data][index]);
+    }
+
+    return ret;
+}
+
 /**
  * Send memcached flush command
  */
@@ -149,6 +160,7 @@ window.addEventListener('load', function() {
         template: '#modal-template'
     });
 
+    // Tooltips borrowed from: https://github.com/bananastalktome/memcached_stats
     app = new Vue({
         el: '#app',
         data: {
@@ -160,27 +172,201 @@ window.addEventListener('load', function() {
                             ticks: {beginAtZero: true}
                         }
                     ]
+                },
+                elements: {
+                    line: {
+                        tension: 0 // No Beziers curve
+                    }
                 }
             },
             chartUpdateConfig: {
-                duration: 0,
-                easing: 'easeOutBounce'
+                duration: 0
             },
             showModal: false,
             flushMemcached: flushMemcached,
-            chartData: {
-                instantHits: getEmptyDataset('Instant Get Hits'),
-                instantMisses: getEmptyDataset('Instant Get Misses'),
-                instantGetflushed: getEmptyDataset('Instant Get Flushed'),
-                instantGetexpired: getEmptyDataset('Instant Get Expired'),
-                instantTouchHits: getEmptyDataset('Instant Touch Hits'),
-                instantEvictions: getEmptyDataset('Instant Evictions'),
-                instantUnfetchedEvicted: getEmptyDataset('Instant Unfetch Evictions'),
-                instantTouchMisses: getEmptyDataset('Instant Touch Misses'),
-                instantDeleteHits: getEmptyDataset('Instant Delete Hits'),
-                instantDeleteMisses: getEmptyDataset('Instant Delete Misses'),
-                instantBytesRead: getEmptyDataset('Instant Bytes Read'),
-                instantBytesWritten: getEmptyDataset('Instant Bytes Written')
+            chartDataGroups: {
+                cmds: {
+                    groupName: 'Commands',
+                    data: [
+                        {
+                            dataset: getEmptyDataset('CMD Get'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'cmd_get',
+                            total: 'N/C',
+                            tooltip: 'Number of retrieval requests'
+                        },
+                        {
+                            dataset: getEmptyDataset('CMD Set'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'cmd_set',
+                            total: 'N/C',
+                            tooltip: 'Number or storage requests'
+                        },
+                        {
+                            dataset: getEmptyDataset('CMD Flush'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'cmd_flush',
+                            total: 'N/C',
+                            tooltip: 'Number of flush requests'
+                        },
+                        {
+                            dataset: getEmptyDataset('CMD Touch'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'cmd_touch',
+                            total: 'N/C',
+                            tooltip: 'Number of touch requests'
+                        }
+                    ]
+                },
+                commands: {
+                    groupName: 'Commands - details',
+                    data: [
+                        {
+                            dataset: getEmptyDataset('Get Hits'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'get_hits',
+                            total: 'N/C',
+                            tooltip: 'Number of keys that have been requested and found present'
+                        },
+                        {
+                            dataset: getEmptyDataset('Get Misses'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'get_misses',
+                            total: 'N/C',
+                            tooltip: 'Number of items that have been requested and not found'
+                        },
+                        {
+                            dataset: getEmptyDataset('Get Flushed'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'get_flushed',
+                            total: 'N/C',
+                            tooltip: ''
+                        },
+                        {
+                            dataset: getEmptyDataset('Get Expired'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'get_expired',
+                            total: 'N/C',
+                            tooltip: 'Number of items that have been requested but were expired'
+                        },
+                        {
+                            dataset: getEmptyDataset('Touch Hits'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'touch_hits',
+                            total: 'N/C',
+                            tooltip: ''
+                        },
+                        {
+                            dataset: getEmptyDataset('Touch Misses'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'touch_misses',
+                            total: 'N/C',
+                            tooltip: ''
+                        },
+                        {
+                            dataset: getEmptyDataset('Delete Hits'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'delete_hits',
+                            total: 'N/C',
+                            tooltip: 'Number of deletion requests resulting in an item being removed'
+                        },
+                        {
+                            dataset: getEmptyDataset('Delete Misses'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'delete_misses',
+                            total: 'N/C',
+                            tooltip: 'Number of deletions requests for missing keys'
+                        },
+                    ]
+                },
+                system: {
+                    groupName: 'System',
+                    data: [
+                        {
+                            dataset: getEmptyDataset('Current Items'),
+                            updateMethod: getRawValues,
+                            updateLabelsMethod: getRawValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'curr_items',
+                            total: 'N/C',
+                            tooltip: 'Current number of items stored'
+                        },
+                        {
+                            dataset: getEmptyDataset('Evicted unfetched'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'evicted_unfetched',
+                            total: 'N/C',
+                            tooltip: ''
+                        },
+                        {
+                            dataset: getEmptyDataset('Evictions'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'evictions',
+                            total: 'N/C',
+                            tooltip: 'Number of valid items removed from cache to free memory for new items'
+                        },
+                    ]
+                },
+                network: {
+                    groupName: 'Network',
+                    data: [
+                        {
+                            dataset: getEmptyDataset('Current connections'),
+                            updateMethod: instantValues,
+                            updateLabelsMethod: instantValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'curr_connections',
+                            total: 'N/C',
+                            tooltip: 'Number of connection structures allocated by the server'
+                        },
+                        {
+                            dataset: getEmptyDataset('Bytes Read (Kb)'),
+                            updateMethod: instantKbValues,
+                            updateLabelsMethod: instantKbValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'bytes_read',
+                            total: 'N/C',
+                            tooltip: 'Total number of bytes read by this server from network'
+                        },
+                        {
+                            dataset: getEmptyDataset('Bytes Written (Kb)'),
+                            updateMethod: instantKbValues,
+                            updateLabelsMethod: instantKbValues,
+                            totalMethod: getLastValue,
+                            memcache_item: 'bytes_written',
+                            total: 'N/C',
+                            tooltip: 'Total number of bytes sent by this server to network'
+                        },
+                    ]
+                }
             }
         },
         mounted() {
@@ -188,56 +374,22 @@ window.addEventListener('load', function() {
             var refresh = function() {
                 axios.get('/stats').then(function (response) {
 
-                    var instantHits = instantValues(response.data, 'get_hits');
-                    root.chartData.instantHits.datasets[0].data = instantHits;
-                    root.chartData.instantHits.labels = instantHits;
+                    // Update dataset values
+                    for (chartGroup in root.chartDataGroups) {
+                        for (groupData in root.chartDataGroups[chartGroup]['data']) {
+                            var memcacheItem = root.chartDataGroups[chartGroup]['data'][groupData]['memcache_item'];
+                            var method = root.chartDataGroups[chartGroup]['data'][groupData]['updateMethod'];
+                            var labelMethod = root.chartDataGroups[chartGroup]['data'][groupData]['updateLabelsMethod'];
+                            var totalMethod = root.chartDataGroups[chartGroup]['data'][groupData]['totalMethod'];
 
-                    var instantMisses = instantValues(response.data, 'get_misses');
-                    root.chartData.instantMisses.datasets[0].data = instantMisses;
-                    root.chartData.instantMisses.labels = instantMisses;
-
-                    var chartdata = instantValues(response.data, 'get_flushed');
-                    root.chartData.instantGetflushed.datasets[0].data = chartdata;
-                    root.chartData.instantGetflushed.labels = chartdata;
-
-                    chartdata = instantKbValues(response.data, 'bytes_read');
-                    root.chartData.instantBytesRead.datasets[0].data = chartdata;
-                    root.chartData.instantBytesRead.labels = chartdata;
-
-                    chartdata = instantKbValues(response.data, 'bytes_written');
-                    root.chartData.instantBytesWritten.datasets[0].data = chartdata;
-                    root.chartData.instantBytesWritten.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'get_expired');
-                    root.chartData.instantGetexpired.datasets[0].data = chartdata;
-                    root.chartData.instantGetexpired.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'touch_hits');
-                    root.chartData.instantTouchHits.datasets[0].data = chartdata;
-                    root.chartData.instantTouchHits.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'touch_misses');
-                    root.chartData.instantTouchMisses.datasets[0].data = chartdata;
-                    root.chartData.instantTouchMisses.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'delete_hits');
-                    root.chartData.instantDeleteHits.datasets[0].data = chartdata;
-                    root.chartData.instantDeleteHits.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'delete_misses');
-                    root.chartData.instantDeleteMisses.datasets[0].data = chartdata;
-                    root.chartData.instantDeleteMisses.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'evicted_unfetched');
-                    root.chartData.instantUnfetchedEvicted.datasets[0].data = chartdata;
-                    root.chartData.instantUnfetchedEvicted.labels = chartdata;
-
-                    chartdata = instantValues(response.data, 'evictions');
-                    root.chartData.instantEvictions.datasets[0].data = chartdata;
-                    root.chartData.instantEvictions.labels = chartdata;
+                            root.chartDataGroups[chartGroup]['data'][groupData]['total'] = totalMethod(response.data, memcacheItem);
+                            root.chartDataGroups[chartGroup]['data'][groupData]['dataset']['labels'] = labelMethod(response.data, memcacheItem);
+                            root.chartDataGroups[chartGroup]['data'][groupData]['dataset']['datasets'][0]['data'] = method(response.data, memcacheItem);
+                        }
+                    }
                 });
             };
-            setInterval(refresh, 3000);
+            setInterval(refresh, 5000);
         }
     });
 });
